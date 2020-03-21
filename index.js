1f3a36d82497624fca3bf77f73ba0694dced4ea9
Deryck H <dhenson@viztek.net>
2020-03-21 01:20:56 -0400

    export a default

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ export * as Loader from './Loader';
 export * as InputWrap from './InputWrap';
 export * as Notice from './Notice';
 export * as ErrorCatcher from './ErrorCatcher';
+
+export default {
+    ...Loader,
+    ...InputWrap,
+    ...Notice,
+    ...ErrorCatcher,
+};
