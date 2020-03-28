f3f7500595e7a767e1ca1a685f58fc11861a8ffe
Deryck H <dhenson@viztek.net>
2020-03-28 19:22:43 -0400

    this pretty much solid now time to move on to the next thing.  siteId no longer required everywhere - will deal with that if it ever comes up

--- statics/Buttons.js
+++ statics/Buttons.js
@@ -1,10 +1,17 @@
 'use strict';
 
+import { Button } from '@smooth-ui/core-sc';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import {
     t,
 } from '../../../data/state';
+import {
+    buttonClass,
+    buttonTextClass,
+    largeButtonClass,
+} from '../../../util/helper';
 
 export const NextButton = props => (
     <button
@@ -23,20 +30,71 @@ export const PreviousButton = ( props ) => (
     </button>
 );
 
-const submitText = (
-    <React.Fragment>
-        <i className={`tw-py-3 tw-px-2 fa fa-check-circle`} /> {t(`kiosk.buttons.submit`)}
-    </React.Fragment>
-);
+// const submitText = (
+//     <React.Fragment>
+//         <i className={`tw-py-3 tw-px-2 fa fa-check-circle`} /> {t(`kiosk.buttons.submit`)}
+//     </React.Fragment>
+// );
+//
+// export const SubmitButton = props => (
+//     <button
+//         type={`submit`}
+//         className={`tw-px-4 tw-text-2xl tw-bg-green tw-text-white tw-rounded`}
+//         onClick={props.handleClick}>
+//         {!props.submitting
+//             ? submitText
+//             : <i className={`tw-py-3 tw-px-2 fa fa-spinner spinner-icon tw-text-orange`} />
+//         }
+//     </button>
+// );
 
-export const SubmitButton = props => (
-    <button
-        type={`submit`}
-        className={`tw-px-4 tw-text-2xl tw-bg-green tw-text-white tw-rounded`}
-        onClick={props.handleClick}>
-        {!props.submitting
-            ? submitText
-            : <i className={`tw-py-3 tw-px-2 fa fa-spinner spinner-icon tw-text-orange`} />
-        }
-    </button>
-);
+export function FormButtonGroup ( props ) {
+    const {
+        className,
+        disabled,
+        handleReset = () => {},
+    } = props;
+
+    return (
+        <div
+            className={`tw-w-full tw-text-center`}
+            key={`buttons-group-wrap-key`}>
+            <Button
+                key={`submit-button-key`}
+                variant={`primary`}
+                className={`tw-w-1/3 tw-mx-2 ${className}`}
+                disabled={disabled}
+                name={`submit-button`}
+                id={`submit-button`}
+                type={`submit`}>
+                Save
+            </Button>
+            <Button
+                key={`reset-button-key`}
+                variant={`secondary`}
+                className={`tw-w-1/3 tw-mx-2 ${className}`}
+                disabled={disabled}
+                name={`reset-button`}
+                id={`reset-button`}
+                type={`button`}
+                onClick={handleReset}>
+                Reset
+            </Button>
+            <div
+                className={`tw-w-full tw-text-center`}
+                key={`home-button-group-wrap-key`}>
+                <Link to={`/home`}>
+                    <Button
+                        key={`home-button-key`}
+                        variant={`info`}
+                        className={`tw-w-1/5 tw-mx-auto ${className}`}
+                        name={`home-button`}
+                        id={`home-button`}
+                        type={`button`}>
+                        Home
+                    </Button>
+                </Link>
+            </div>
+        </div>
+    )
+}
